222e337b6d882234b87eaac6304c693b623af982
sumon550 <entertainmentcharacter54321@gmail.com>
2026-05-19 09:08:46 +0600

    Add navbar section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-// import Navbar from "@/components/Navbar";
+import Navbar from "@/components/Navbar";
 // import Footer from "@/components/Footer";
 
 const geistSans = Geist({
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        {/* <Navbar></Navbar> */}
+        <Navbar></Navbar>
         <main>{children}</main>
         {/* <Footer></Footer> */}
       </body>
