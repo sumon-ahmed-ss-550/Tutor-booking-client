32945d4b4ab71838152741f2e36d568b4671b936
sumon550 <entertainmentcharacter54321@gmail.com>
2026-05-19 09:10:30 +0600

    Add carousel section

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
-// import Carousel from "@/components/Carousel";
+import Carousel from "@/components/Carousel";
 
 export default function Home() {
-  return <div>{/* <Carousel></Carousel> */}</div>;
+  return <div>{<Carousel></Carousel>}</div>;
 }
