1f1970e29dbc0c4c01a3970dd0f308d2e29aa3bc
sumon550 <entertainmentcharacter54321@gmail.com>
2026-05-24 10:34:47 +0600

    Fix delete functionality

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -7,9 +7,12 @@ const Table = ({ data }) => {
   const router = useRouter();
 
   const handleDeleteData = async (userId) => {
-    const res = await fetch(`${NEXT_PUBLIC_SERVER_URL}/tutors/user/${userId}`, {
-      method: "DELETE",
-    });
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/tutors/user/${userId}`,
+      {
+        method: "DELETE",
+      },
+    );
     const data = await res.json();
     if (data) {
       router.refresh();
